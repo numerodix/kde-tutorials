d4368ac8099cf50925e6295d4547a5d715bda485
Martin Matusiak <numerodix@gmail.com>
2019-12-08 15:58:25 +1100

    add saving-and-loading

---
+++ saving-and-loading/src/mainwindow.h
@@ -0,0 +1,33 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+ 
+#include <KXmlGuiWindow>
+
+class KTextEdit;
+class KJob;
+ 
+class MainWindow : public KXmlGuiWindow
+{
+    Q_OBJECT
+    
+  public:
+    explicit MainWindow(QWidget *parent = nullptr);
+ 
+  private:
+    void setupActions();
+ 
+  private slots:
+    void newFile();
+    void openFile();
+    void saveFile();
+    void saveFileAs();
+    void saveFileAs(const QString &outputFileName);
+    
+    void downloadFinished(KJob* job);
+
+  private:
+    KTextEdit* textArea;
+    QString fileName;
+};
+ 
+#endif
